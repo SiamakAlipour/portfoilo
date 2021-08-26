0813d565cf060a6478ea3adbccef69c0cd4e0364
Siamak Alipour <siamakalipour37@gmail.com>
2021-08-26 17:20:59 +0430

    Initial dist subtree commit

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -1,8 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Slideoptions from './Slideoptions'
 import './Slide.scss'
 import { useSelector, useDispatch } from 'react-redux'
 import { setSlideIndex } from '../store/actions/actions'
+import { Link } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+
 function Slide() {
 	const slide = useSelector((state) => state.slide)
 	const dispatch = useDispatch()
@@ -14,16 +17,19 @@ function Slide() {
 				active={slide}
 				onClick={() => dispatch(setSlideIndex(0))}
 			/>
+
 			<Slideoptions
 				index={1}
 				active={slide}
 				onClick={() => dispatch(setSlideIndex(1))}
 			/>
+
 			<Slideoptions
 				index={2}
 				active={slide}
 				onClick={() => dispatch(setSlideIndex(2))}
 			/>
+
 			<Slideoptions
 				index={3}
 				active={slide}
--- src/projectsData.js
+++ src/projectsData.js
@@ -1,12 +1,14 @@
 export const projectsData = [
 	{
-		name: 'test1',
+		name: 'TodoList',
 		language: 'Python',
 		description: 'this is a default description for test1',
+		github: 'https://www.google.com',
 	},
 	{
 		name: 'test2',
 		language: 'JavaScript',
 		description: 'this is a default description for test2',
+		github: 'https://www.google.com',
 	},
 ]
